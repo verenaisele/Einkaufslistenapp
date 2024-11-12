fce1079afabd0e9fb827a97fc18793971c2ecb4a
verenaisele <77627438+verenaisele@users.noreply.github.com>
2024-11-12 17:45:42 +0100

    weiteres styling

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+const saveBtn = document.getElementById("save-btn")
+const inputEl = document.getElementById("input-el")
+const listEl = document.getElementById("list-el")
+
+saveBtn.addEventListener("click", function(){
+    listEl.innerHTML += `<li>${inputEl.value}</li>`
+    inputEl.value = ""
+})
