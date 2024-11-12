a47d7c385928df166b46a6433387e9343248d8f2
verenaisele <77627438+verenaisele@users.noreply.github.com>
2024-11-12 17:59:20 +0100

    basic js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const saveBtn = document.getElementById("save-btn")
 const inputEl = document.getElementById("input-el")
 const listEl = document.getElementById("list-el")
+const deleteBtn = document.getElementById("delete-btn")
+let list = []
 
 saveBtn.addEventListener("click", function(){
-    listEl.innerHTML += `<li>${inputEl.value}</li>`
+    list.push(inputEl.value)
+    let listItem = ""
+    for (let i = 0; i < list.length; i++) {
+      listItem += `<li>${list[i]}</li>`
+    }
+    listEl.innerHTML = listItem
     inputEl.value = ""
+})
+
+deleteBtn.addEventListener('dblclick', function(){
+    
 })
